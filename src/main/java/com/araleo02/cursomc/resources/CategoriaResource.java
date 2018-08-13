1335b55c98af89e759b79f53de54811578a7a181
Leonardo Gregorio <leonard.gregorio@gmail.com>
2018-08-13 10:37:52 -0300

    metodo post categorias

--- src/main/java/com/araleo02/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/araleo02/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.araleo02.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.araleo02.cursomc.domain.Categoria;
 import com.araleo02.cursomc.services.CategoriaService;
@@ -17,10 +21,18 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 
+	// metodo get para trazer algo, feito na aula de UML
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
 
+	// metodo post para salvar um item na categoria 
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
--- src/main/java/com/araleo02/cursomc/teste.java
+++
@@ -1,5 +0,0 @@
-package com.araleo02.cursomc;
-
-public class teste {
-
-}
