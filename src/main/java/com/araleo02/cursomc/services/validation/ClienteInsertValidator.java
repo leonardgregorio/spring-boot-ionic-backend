98b4389e6f9475aa1ef1d336a860804ce68200f8
Leonardo Gregorio <leonard.gregorio@gmail.com>
2018-08-14 17:00:45 -0300

    validacao customizada de cpf ou cnpj na insercao de cliente

---
+++ src/main/java/com/araleo02/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,37 @@
+package com.araleo02.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.araleo02.cursomc.domain.enums.TipoCliente;
+import com.araleo02.cursomc.dto.ClienteNewDTO;
+import com.araleo02.cursomc.resources.exception.FieldMessage;
+import com.araleo02.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("CpfOuCnpj", "Campo CPF invalido!"));
+		}
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSAJURIDICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("CpfOuCnpj", "Campo CPF invalido!"));
+		}		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
