9baab293bcc55d6fe3c8e186d93a57dfb86923c0
leonard.gregorio <leonard.gregorio@gmail.com>
2018-09-25 21:42:21 -0300

    Implementando autorizacao

--- src/main/java/com/araleo02/cursomc/config/SecurityConfig.java
+++ src/main/java/com/araleo02/cursomc/config/SecurityConfig.java
@@ -21,6 +21,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.araleo02.cursomc.security.JWTAuthenticationFilter;
+import com.araleo02.cursomc.security.JWTAuthorizationFilter;
 import com.araleo02.cursomc.security.JWTUtil;
 
 @Configuration
@@ -51,6 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
