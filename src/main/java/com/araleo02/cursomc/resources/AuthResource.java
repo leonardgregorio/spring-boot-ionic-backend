2525e18cf890eeca1d3029e8350387e273cf8bcc
leonard.gregorio <leonard.gregorio@gmail.com>
2018-10-02 16:29:50 -0300

    refresh token

---
+++ src/main/java/com/araleo02/cursomc/resources/AuthResource.java
@@ -0,0 +1,32 @@
+//aula 74. Refresh token
+
+package com.araleo02.cursomc.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.araleo02.cursomc.security.JWTUtil;
+import com.araleo02.cursomc.security.UserSS;
+import com.araleo02.cursomc.services.UserService;
+
+@RestController
+@RequestMapping(value="/auth")
+public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@RequestMapping(value="/refresh_token", method=RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+	UserSS user = UserService.authenticated();
+	String token = jwtUtil.generateToken(user.getUsername());
+	response.addHeader("Authorization", "Bearer " + token);
+	return ResponseEntity.noContent().build();
+	}
+
+}
