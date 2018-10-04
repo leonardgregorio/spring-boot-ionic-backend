909f9538f9537b695b4bf63bd5f546cb3b6ae3e0
leonard.gregorio <leonard.gregorio@gmail.com>
2018-10-04 13:03:01 -0300

    salvando primeiro arquivo no s3

--- src/main/java/com/araleo02/cursomc/CursomcApplication.java
+++ src/main/java/com/araleo02/cursomc/CursomcApplication.java
@@ -1,12 +1,18 @@
 package com.araleo02.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.araleo02.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -14,5 +20,7 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		//Aula 81. Salvando primeiro arquivo no S3
+		s3Service.uploadFile("C:\\Users\\araleo02\\Downloads\\Foto do sensor.jpeg");
 	}
 }
